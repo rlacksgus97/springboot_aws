a9b69ca523cc8d14ba16006c4b5e13e7cf064e9a
rlacksgus97 <rlacksgus97@naver.com>
2021-07-08 23:02:31 +0900

    Chapter 02. 스프링 부트에서 테스트 코드를 작성하자

---
+++ src/main/java/com/practice/spring/aws/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.practice.spring.aws.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
