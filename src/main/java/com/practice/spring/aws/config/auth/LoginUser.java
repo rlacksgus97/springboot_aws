fa772b8e1f2ceef3399f626c4b3ae666759ca00b
rlacksgus97 <rlacksgus97@naver.com>
2021-07-23 18:42:56 +0900

    Chapter 05. 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기

---
+++ src/main/java/com/practice/spring/aws/config/auth/LoginUser.java
@@ -0,0 +1,11 @@
+package com.practice.spring.aws.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
--- src/main/java/com/practice/spring/aws/web/IndexController.java
+++ src/main/java/com/practice/spring/aws/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.practice.spring.aws.web;
 
+import com.practice.spring.aws.config.auth.LoginUser;
+import com.practice.spring.aws.config.auth.dto.SessionUser;
 import com.practice.spring.aws.service.posts.PostsService;
 import com.practice.spring.aws.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -32,5 +40,4 @@ public class IndexController {
 
         return "posts-update";
     }
-
 }
